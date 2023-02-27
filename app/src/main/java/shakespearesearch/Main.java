a676981986e6990d3b8abd2771a2e7a365f705a9
webecker25272 <wilsonebecker@gmail.com>
2023-02-26 20:06:34 -0500

    change search term

--- app/src/main/java/shakespearesearch/Main.java
+++ app/src/main/java/shakespearesearch/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         String fileName = "/shakespeare.txt";
-        String searchTerm = "romeo";
+        String searchTerm = "i";
         int numThreads = 4;
 
         ShakespeareSearch search = new ShakespeareSearch(fileName, searchTerm, numThreads);
