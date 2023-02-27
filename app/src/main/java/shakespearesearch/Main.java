62a386d9891c23e7429a828e3778dd3cbe49c24b
webecker25272 <wilsonebecker@gmail.com>
2023-02-26 20:39:37 -0500

    changing interface for ShakespeareSearch

--- app/src/main/java/shakespearesearch/Main.java
+++ app/src/main/java/shakespearesearch/Main.java
@@ -1,22 +1,11 @@
 package shakespearesearch;
 
-import java.io.IOException;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         String fileName = "/shakespeare.txt";
-        String searchTerm = "i";
+        String searchTerm = "example phrase";
         int numThreads = 4;
 
-        ShakespeareSearch search = new ShakespeareSearch(fileName, searchTerm, numThreads);
-        try {
-            List<ShakespeareSearch.Match> matches = search.search();
-            for (ShakespeareSearch.Match match : matches) {
-                System.out.println("Match found on line " + match.getLineNumber() + ": " + match.getLineText());
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        ShakespeareSearch search = new ShakespeareSearch();
     }
 }
