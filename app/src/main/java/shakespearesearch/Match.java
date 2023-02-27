5c8b7db14db1a60a0c15482d5631c5ec0820f2e5
webecker25272 <wilsonebecker@gmail.com>
2023-02-26 20:30:10 -0500

    putting match.java back in

---
+++ app/src/main/java/shakespearesearch/Match.java
@@ -0,0 +1,19 @@
+package shakespearesearch;
+
+public class Match {
+    private final int lineNumber;
+    private final String line;
+
+    public Match(int lineNumber, String line) {
+        this.lineNumber = lineNumber;
+        this.line = line;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public String getLine() {
+        return line;
+    }
+}
