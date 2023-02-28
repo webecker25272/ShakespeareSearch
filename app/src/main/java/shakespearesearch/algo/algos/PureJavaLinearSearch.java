cf37577b076531c14c92a6f81175d85b58e8c9e8
webecker25272 <wilsonebecker@gmail.com>
2023-02-27 21:09:04 -0500

    fixing PJLS

--- app/src/main/java/shakespearesearch/algo/algos/PureJavaLinearSearch.java
+++ app/src/main/java/shakespearesearch/algo/algos/PureJavaLinearSearch.java
@@ -4,22 +4,27 @@ import shakespearesearch.algo.Algo;
 import shakespearesearch.utils.threading.Chunk;
 import shakespearesearch.utils.threading.Match;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PureJavaLinearSearch extends Algo {
-    private static final int CHUNK_SIZE = 1000;
 
     public PureJavaLinearSearch() {
         super("Pure Java Linear Search");
     }
 
     @Override
-    public List<Match> searchChunk(Chunk chunks, String searchTerm) {
-        // search each chunk for matches
-        int numMatches = 0;
-        for (Chunk chunk : chunks) {
-            numMatches += searchChunk(chunk, searchTerm).size();
+    public List<Match> searchChunk(Chunk chunk, String searchTerm) {
+        List<Match> matches = new ArrayList<>();
+        int startIndex = chunk.getStartIndex();
+        List<String> lines = chunk.getLines();
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            if (line.contains(searchTerm)) {
+                int lineNumber = startIndex + i;
+                matches.add(new Match(lineNumber, line));
+            }
         }
-        return null;
+        return matches;
     }
 }
