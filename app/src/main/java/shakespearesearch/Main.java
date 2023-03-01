a9f4848fca4137a90c0f88848febeeb668202073
webecker25272 <wilsonebecker@gmail.com>
2023-02-28 22:24:37 -0500

    adding backend server

--- app/src/main/java/shakespearesearch/Main.java
+++ app/src/main/java/shakespearesearch/Main.java
@@ -1,25 +1,38 @@
 package shakespearesearch;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 import shakespearesearch.algo.Algo;
 import shakespearesearch.algo.algos.PureJavaLinearSearch;
 import shakespearesearch.utils.eval.AlgoResult;
 import shakespearesearch.utils.eval.Iterator;
+import shakespearesearch.backendserver.BackendServer;
 
 import java.util.List;
 
+@SpringBootApplication
+@ComponentScan({"shakespearesearch", "backendserver"})
 public class Main {
     private static final boolean PRINTLINE = true;
     private static final int MAXTHREADS = 10;
     private static final String RESOURCENAME = "/shakespeare.txt";
     private static final String SEARCHTERM = " test ";
 
-    public static void main(String[] args) throws InterruptedException {        
+    public static void main(String[] args) throws InterruptedException {     
+        String fileContent = Server.getFileContent("shakespeare.txt");
+
         Iterator iterator = new Iterator();
 
         Algo PJLS = new PureJavaLinearSearch();
         List<AlgoResult> pjlsResult = iterator.iterate(PJLS, RESOURCENAME, SEARCHTERM, MAXTHREADS, PRINTLINE); //this is what gets displayed to the front end
         
-
+        //new algos go here
     
+
+
+
+        SpringApplication.run(Main.class, args);
     }
 }
